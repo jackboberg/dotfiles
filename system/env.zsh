677e5692435ed17590ec59af030dba1d370a1f76
Jack Boberg <jack.boberg@gmail.com>
2012-05-13 17:17:51 -0400

    import old bash config to relevent system topic files

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,18 @@
-export EDITOR='subl'
+export EDITOR='vi'
+
+# export TERM=xterm-color
+
+# make tab completion case insensitive
+# bind "set completion-ignore-case on"
+
+# the bash history should save 10000 commands
+# export HISTFILESIZE=10000
+
+# don't put duplicate lines in the history.
+# export HISTCONTROL=ignoredups
+
+# append new lines, don't wipe out history
+# shopt -s histappend
+
+# make history available across shells
+# export PROMPT_COMMAND="history -a; history -n"
--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,21 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+# export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+# export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+# use local FIRST
+LOCAL_PATH=/usr/local/bin:/usr/local/sbin
+
+# zsh bin
+ZSH_PATH = $ZSH/bin
+
+# Python
+PYTHON_PATH=/usr/local/share/python
+
+# Ruby
+RUBY_PATH=/usr/local/Cellar/ruby/1.9.3-p194/bin
+export GEM_HOME='/usr/local'
+
+# Node/NPM
+NPM_PATH=/usr/local/share/npm/bin
+export NODE_PATH=/usr/local/lib/node_modules
+
+export PATH=$LOCAL_PATH:$ZSH_PATH:$PYTHON_PATH:$RUBY_PATH:$NPM_PATH:$PATH
