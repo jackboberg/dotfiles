fce3372d2d5f4be5ac5de6200ef207069bdc63a1
Jack Boberg <jack.boberg@gmail.com>
2012-05-13 21:36:07 -0400

    change prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,10 +1,19 @@
-autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
+# import colors to named vars
+autoload colors
+if [[ "$terminfo[colors]" -gt 8 ]]; then
+    colors
+fi
+for COLOR in RED GREEN YELLOW BLUE MAGENTA CYAN BLACK WHITE; do
+    eval $COLOR='$fg_no_bold[${(L)COLOR}]'
+    eval BOLD_$COLOR='$fg_bold[${(L)COLOR}]'
+done
+eval RESET='$reset_color'
+
 
 git_branch() {
   echo $(/usr/bin/git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
 }
+
 
 git_dirty() {
   st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
@@ -14,39 +23,37 @@ git_dirty() {
   else
     if [[ $st == "nothing to commit (working directory clean)" ]]
     then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+      echo "%{$GREEN%}$(git_prompt_info)%{$RESET%}"
     else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+      echo "%{$RED%}$(git_prompt_info)%{$RESET%}"
     fi
   fi
 }
 
+
 git_prompt_info () {
- ref=$(/usr/bin/git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
+  ref=$(/usr/bin/git symbolic-ref HEAD 2>/dev/null) || return
+  echo "(${ref#refs/heads/})"
 }
+
 
 unpushed () {
   /usr/bin/git cherry -v @{upstream} 2>/dev/null
 }
+
 
 need_push () {
   if [[ $(unpushed) == "" ]]
   then
     echo " "
   else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
+    echo " %{$MAGENTA%}▲%{$RESET%}"
   fi
 }
 
-rb_prompt(){
-  if $(which rbenv &> /dev/null)
-  then
-	  echo "%{$fg_bold[yellow]%}$(rbenv version | awk '{print $1}')%{$reset_color%}"
-	else
-	  echo ""
-  fi
+
+local_user(){
+  echo "%{$CYAN%}%n%{$RESET%}@%{$CYAN%}%m%{$RESET%}"
 }
 
 # This keeps the number of todos always available the right hand side of my
@@ -55,7 +62,7 @@ rb_prompt(){
 todo(){
   if $(which todo.sh &> /dev/null)
   then
-    num=$(echo $(todo.sh ls +next | wc -l))
+    num=$(echo $(td ls +next | wc -l))
     let todos=num-2
     if [ $todos != 0 ]
     then
@@ -68,15 +75,16 @@ todo(){
   fi
 }
 
+
 directory_name(){
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+  echo "%{$BLUE%}%~%{$RESET%}"
 }
 
-export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
+
+export PROMPT=$'\n[$(local_user):$(directory_name)] $(git_dirty)$(need_push)\n› '
 set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
+  export RPROMPT="%{$GREEN%}$(todo)%{$RESET%}"
 }
-
 precmd() {
   title "zsh" "%m" "%55<...<%~"
   set_prompt
