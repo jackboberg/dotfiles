5598b9b8d3fa0a8090862e6f2cde240b2143b871
Jack Boberg <jack.boberg@gmail.com>
2012-05-13 22:14:17 -0400

    update system topic to personal aliases reanme to _system so it loads first

---
+++ _system/aliases.zsh
@@ -0,0 +1,20 @@
+# Better list output
+alias l='ls -lFGh'
+alias ll='ls -lAFGh'
+
+# Traversing
+alias ..='cd ..'
+alias ...='cd ../..'
+
+# Super user
+alias _='sudo'
+
+# History
+alias h='history'
+alias fh='history | grep $1' #Requires one input
+
+# How deep into vim have I gotten
+alias shlvl="echo $SHLVL"
+
+# Clear the DNS
+alias flush_dns="dscacheutil -flushcache"
