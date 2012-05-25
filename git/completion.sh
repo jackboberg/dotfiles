b2dd3ada87f683986a650923e59b6627cb917885
Jack Boberg <jack.boberg@gmail.com>
2012-05-25 15:24:52 -0400

    use zsh git completion, not bash's!

--- git/completion.sh
+++ git/completion.sh
@@ -1,8 +1,10 @@
-# Uses git's autocompletion for inner commands. Assumes an install of git's
-# bash `git-completion` script at $completion below (this is where Homebrew
-# tosses it, at least).
-completion=/usr/local/etc/bash_completion.d/git-completion.bash
+completion=/usr/local/share/zsh/functions/_git
+if test -f $completion
+then
+  source $completion
+fi
 
+completion=/usr/local/share/zsh/functions/git-flow-completion.zsh
 if test -f $completion
 then
   source $completion
