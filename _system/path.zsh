2014a9daebb08986edde0afc3aa456d6d145808f
Jack Boberg <jack.boberg@gmail.com>
2012-06-06 16:15:12 -0400

    update PATH to include MAMP PHP

--- _system/path.zsh
+++ _system/path.zsh
@@ -4,6 +4,9 @@
 # use local FIRST
 LOCAL_PATH=/usr/local/bin:/usr/local/sbin
 
+# PHP
+PHP_PATH=/Applications/MAMP/bin/php/php5.3.6/bin
+
 # Python
 PYTHON_PATH=/usr/local/share/python
 
@@ -15,4 +18,4 @@ export GEM_HOME='/usr/local'
 NPM_PATH=/usr/local/share/npm/bin
 export NODE_PATH=/usr/local/lib/node_modules
 
-export PATH=$LOCAL_PATH:$ZSH/bin:$PYTHON_PATH:$RUBY_PATH:$NPM_PATH:$PATH
+export PATH=$LOCAL_PATH:$ZSH/bin:$PHP_PATH:$PYTHON_PATH:$RUBY_PATH:$NPM_PATH:$PATH
