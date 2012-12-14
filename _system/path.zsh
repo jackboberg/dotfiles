8e225835b9d02ff21f2e7665126906a1db7dbee2
Jack Boberg <jack.boberg@gmail.com>
2012-12-14 11:24:46 -0500

    remove MAMP PHP from $PATH
    clean up old comments

--- _system/path.zsh
+++ _system/path.zsh
@@ -1,11 +1,5 @@
-# export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-# export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
 # use local FIRST
 LOCAL_PATH=/usr/local/bin:/usr/local/sbin
-
-# PHP
-PHP_PATH=/Applications/MAMP/bin/php/php5.3.6/bin
 
 # Python
 PYTHON_PATH=/usr/local/share/python
@@ -17,4 +11,4 @@ RUBY_PATH=$HOME/.rbenv/shims:$HOME/.rbenv/bin
 NPM_PATH=/usr/local/share/npm/bin
 export NODE_PATH=/usr/local/lib/node_modules
 
-export PATH=.bin:$RUBY_PATH:$LOCAL_PATH:$ZSH/bin:$PHP_PATH:$PYTHON_PATH:$NPM_PATH:$PATH
+export PATH=./.bin:./bin:$RUBY_PATH:$LOCAL_PATH:$ZSH/bin:$PYTHON_PATH:$NPM_PATH:$PATH
