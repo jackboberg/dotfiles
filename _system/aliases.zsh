8f3a04b4ad96bc4a5cffebbe6b81cee326745c96
Jack Boberg <jack.boberg@gmail.com>
2013-02-21 12:16:14 -0500

    add alias for fixing osx 'Open With' duplicates

--- _system/aliases.zsh
+++ _system/aliases.zsh
@@ -26,3 +26,7 @@ alias flush_dns="dscacheutil -flushcache"
 
 # print current IP
 alias myip='curl -s http://checkrealip.com/ | grep "Current IP Address"'
+
+# Quick way to rebuild the Launch Services database and get rid
+# of duplicates in the Open With submenu.
+alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
