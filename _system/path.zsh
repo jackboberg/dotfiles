6f58b60048dbf540e0e8b97d94f719629fba7677
Jack Boberg <jack.boberg@gmail.com>
2013-09-05 14:32:25 -0400

    change NODE_PATH

--- _system/path.zsh
+++ _system/path.zsh
@@ -7,8 +7,7 @@ PYTHON_PATH=/usr/local/share/python
 # Ruby
 RUBY_PATH=$HOME/.rbenv/shims:$HOME/.rbenv/bin
 
-# Node/NPM
+# Node/npm
 NPM_PATH=/usr/local/share/npm/bin
-export NODE_PATH=/usr/local/lib/node_modules
 
 export PATH=./.bin:./bin:$RUBY_PATH:$LOCAL_PATH:$ZSH/bin:$PYTHON_PATH:$NPM_PATH:$PATH
