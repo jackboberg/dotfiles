3c182b866b6558e50466a88d76efc55ec4f895aa
Jack Boberg <jack.boberg@gmail.com>
2013-09-05 14:33:48 -0400

    update git completion, config, and global ignore

--- git/completion.sh
+++ git/completion.sh
@@ -1,11 +1,17 @@
-completion=/usr/local/share/zsh/functions/_git
-if test -f $completion
-then
-  source $completion
-fi
+# completion=/usr/local/share/zsh/site-functions/_git
+# if test -f $completion
+# then
+#   source $completion
+# fi
+# 
+# completion=/usr/local/share/zsh/site-functions/git-flow-completion.zsh
+# if test -f $completion
+# then
+#   source $completion
+# fi
 
-completion=/usr/local/share/zsh/functions/git-flow-completion.zsh
-if test -f $completion
-then
-  source $completion
-fi
+# completion=/usr/local/share/zsh/site-functions/_hub
+# if test -f $completion
+# then
+#   source $completion
+# fi
