160bff6d02718e99c278f1dd26d05f0e8d290ff1
Jack Boberg <jack.boberg@gmail.com>
2014-08-02 15:31:04 -0400

    Set basic system aliases and update the $PATH

---
+++ _system/aliases.zsh
@@ -0,0 +1,7 @@
+# Better list output
+alias l='ls -lFGh'
+alias ll='ls -lAFGh'
+
+# Traversing
+alias ..='cd ..'
+alias ...='cd ../..'
---
+++ _system/path.zsh
@@ -0,0 +1,5 @@
+# even more local bin directories
+BIN_PATH=./.bin:./bin:$HOME/.bin:$DOT/bin
+
+# export the updated path
+export PATH=$BIN_PATH:$PATH
