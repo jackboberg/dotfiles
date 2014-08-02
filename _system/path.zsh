db571934e0fee040609011262a4334e6e18f9035
Jack Boberg <jack.boberg@gmail.com>
2014-08-02 15:45:12 -0400

    Setup rbenv with completion
    rbenv installed with homebrew

--- _system/path.zsh
+++ _system/path.zsh
@@ -1,5 +1,9 @@
+# Ruby
+export RBENV_ROOT=/usr/local/var/rbenv
+RUBY_PATH=$RBENV_ROOT/shims
+
 # even more local bin directories
 BIN_PATH=./.bin:./bin:$HOME/.bin:$DOT/bin
 
 # export the updated path
-export PATH=$BIN_PATH:$PATH
+export PATH=$BIN_PATH:$RUBY_PATH:$PATH
---
+++ ruby/rbenv.completion.sh
@@ -0,0 +1,1 @@
+source /usr/local/Cellar/rbenv/0.4.0/completions/rbenv.zsh
