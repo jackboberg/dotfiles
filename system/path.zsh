340e544a9272776ab9ea58e0aeee0a5b2f0b5f4f
Jack Boberg <hello@jackboberg.info>
2017-08-11 15:25:02 -0400

    fix: Remove duplicate rbenv PATH

--- system/path.zsh
+++ system/path.zsh
@@ -1,9 +1,3 @@
 # even more local bin directories
 BIN_PATH=./.bin:./bin:$HOME/.bin:$DOT/bin
-#
-# Ruby
-export RBENV_ROOT=$(brew --prefix)/var/rbenv
-RUBY_PATH=$RBENV_ROOT/shims
-
-# export the updated path
-export PATH=$BIN_PATH:$RUBY_PATH:$PATH
+export PATH=$BIN_PATH:$PATH
