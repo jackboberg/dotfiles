0fc284117818758ec1e1f1012d083ccb3a217b1c
Jack Boberg <hello@jackboberg.info>
2019-10-09 10:45:54 +0200

    Import usefule git aliases from `.dmz`
    - remove legacy files

--- zsh/git/alias.zsh
+++ zsh/git/alias.zsh
@@ -6,5 +6,13 @@ then
   alias git=$hub_path
 fi
 
-# The rest of my fun git aliases
-alias glog="git --no-pager log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative -15"
+alias glog="git --no-pager log \
+--graph \
+--pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' \
+--abbrev-commit \
+--date=relative \
+-15"
+alias gc='git commit'
+alias gco='git checkout'
+alias gd='git diff'
+alias gs='git status -sb'
