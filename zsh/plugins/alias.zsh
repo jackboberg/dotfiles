f53d7f3d23151bdfdce9c61433c3ae9540bdb5de
Jack Boberg <hello@jackboberg.info>
2020-09-15 23:09:32 +0200

    refactor: Move `mkd` to utilites and add `p`
    - `p` replaces my most common use of `enhancd`

--- zsh/plugins/alias.zsh
+++ zsh/plugins/alias.zsh
@@ -3,8 +3,3 @@ alias e='emacs -nw'
 # exa
 alias l="exa -l"
 alias ll="exa -la"
-
-# create directory and change to it
-mkd () {
-    mkdir -pv "${1}" && cd "${1}"
-}
--- zsh/plugins/utils.zsh
+++ zsh/plugins/utils.zsh
@@ -3,3 +3,13 @@ alias myip="curl -s https://api.myip.com | jq -r '{ip} | (.ip)'"
 
 # Copy my public key to my clipboard
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+
+# create directory and change to it
+mkd () {
+    mkdir -pv "${1}" && cd "${1}"
+}
+
+# cd to fuzzy-found locally cloned repository
+p () {
+    cd $(ghq root)/$(ghq list | fzy);
+}
