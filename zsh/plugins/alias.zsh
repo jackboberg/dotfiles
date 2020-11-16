d59c2bf02ebfabba0ea69ee628dba927d907d028
Jack Boberg <hello@jackboberg.info>
2020-11-16 10:31:29 +0100

    feat: Setup rails environment aliases

--- zsh/plugins/alias.zsh
+++ zsh/plugins/alias.zsh
@@ -3,3 +3,8 @@ alias e='emacs -nw'
 # exa
 alias l="exa -l"
 alias ll="exa -la"
+
+# rails env
+alias -g RED='RAILS_ENV=development'
+alias -g REP='RAILS_ENV=production'
+alias -g RET='RAILS_ENV=test'
