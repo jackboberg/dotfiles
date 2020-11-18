801ba08c49cfa1bb4848d391dd0765076c863567
Jack Boberg <hello@jackboberg.info>
2020-11-19 00:23:45 +0100

    feat: Include chemacs configuration

---
+++ emacs-profiles.el
@@ -0,0 +1,5 @@
+(
+ ("default" . ((user-emacs-directory . "~/.emacs.d")))
+ ("spacemacs" . ((user-emacs-directory . "~/src/github.com/syl20bnr/spacemacs")))
+ ("doom" . ((user-emacs-directory . "~/src/github.com/hlissner/doom-emacs")))
+)
