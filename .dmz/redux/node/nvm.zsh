4f77cadd5f3c032653ffe2e440b2ed1000d61439
Jack Boberg <hello@jackboberg.info>
2020-11-19 00:03:52 +0100

    chore: Clean up `redux` configuration
    - trim duplicate homebrew list
    - remove legacy config
    - import docker zsh plugin/aliases

--- .dmz/redux/node/nvm.zsh
+++
@@ -1,4 +0,0 @@
-export NVM_DIR=/usr/local/opt/nvm
-
-# lukechilds/zsh-nvm
-# export NVM_LAZY_LOAD=true
--- .dmz/redux/system/aliases.zsh
+++
@@ -1,7 +0,0 @@
-# Better list output
-alias l='exa -l'
-alias ll='exa -la'
-
-alias e="emacs -nw"
-# alias e="emacsclient -t -a ''"
-# alias kill-emacs="emacsclient -e '(kill-emacs)'"
