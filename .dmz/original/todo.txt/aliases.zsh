9fd9fcd80e94a9f7271114a75e9cbee6851e87b3
Jack Boberg <hello@jackboberg.info>
2020-11-25 01:42:55 +0100

    chore: Remove todo.sh configuration

--- .dmz/original/todo.txt/aliases.zsh
+++
@@ -1,4 +0,0 @@
-# todo.sh: https://github.com/ginatrapani/todo.txt-cli
-
-alias t='todo.sh -d ~/Dropbox/Documents/todo/.todo.personal.cfg'
-alias td='todo.sh -d ~/Dropbox/Documents/todo/.todo.work.cfg'
