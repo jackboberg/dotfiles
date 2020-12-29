5f11e4132dcef05e6dee2aa3fc99258917fa8105
Jack Boberg <hello@jackboberg.info>
2020-12-29 23:52:20 +0100

    feat: Replace `hub` with `gh` aliases

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,11 +1,3 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if [[ -f $hub_path ]]
-then
-  alias git=$hub_path
-fi
-
 alias glog="git --no-pager log \
 --graph \
 --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' \
