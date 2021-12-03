0e0f1587f330e33f6452ca89eb0326d6ef179edb
Jack Boberg <hello@jackboberg.info>
2021-12-03 21:18:00 +0100

    Disable snippets and org mode
    I'm not currently using org-mode and the snippets seem to keep firing
    when I don't want them to.

--- doom.d/init.el
+++ doom.d/init.el
@@ -59,7 +59,7 @@
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
-       snippets            ; my elves. They type so I don't have to
+       ;;snippets          ; my elves. They type so I don't have to
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
@@ -144,7 +144,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org                 ; organize your plain life in plain text
+       ;;org               ; organize your plain life in plain text
        ;;perl              ; write code no one else can comprehend
        ;;(php +lsp)        ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
