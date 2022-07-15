58b28a4ba939a1ac74c9be7397ea5737d9fb5ca7
Jack Boberg <hello@jackboberg.info>
2022-07-15 17:07:55 +0200

    Add docker aliases from client
    Currently working with a client that has LOTS of containers and this is
    their prescribed process for teardown/cleanup when rebuilding.
    
    Some of this is over aggressive, but is consistent with my current workload.

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -1,1 +1,20 @@
 alias drmi='docker rmi $(docker images --filter "dangling=true" -qa)'
+
+function dclean {
+  echo "Removing dead containers..."
+  docker container rm $(docker container ls -a -q -f status=dead) 2>/dev/null || echo "...No dead containers to remove."
+
+  echo "Removing stopped containers..."
+  docker rm $(docker ps --all --quiet --filter status=exited) 2>/dev/null|| echo "...No stopped containers to remove."
+
+  echo "Removing untagged images..."
+  docker rmi $(docker images --quiet --filter dangling=true) 2>/dev/null || echo "...No dead images to remove."
+}
+
+dstop () {
+  # echo "Stopping docker-sync..."
+  # docker-sync stop
+
+  echo "Stopping all docker containers..."
+  docker stop $(docker ps --all --quiet)
+}
