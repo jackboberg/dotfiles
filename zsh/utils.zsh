00ea785142a311d423f75ad63064c076fa40f7d7
Jack Boberg <hello@jackboberg.info>
2022-07-15 16:55:53 +0200

    Add zsh utils

--- zsh/utils.zsh
+++ zsh/utils.zsh
@@ -1,8 +1,11 @@
 # print current IP
 alias myip="curl -s https://api.myip.com | jq -r '{ip} | (.ip)'"
+# curl whatismyip.akamai.com
 
 # Copy my public key to my clipboard
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+
+alias guid="curl -s givemeguid.com"
 
 # create directory and change to it
 mkd () {
@@ -12,4 +15,12 @@ mkd () {
 # cd to fuzzy-found locally cloned repository
 p () {
     cd $(ghq root)/$(ghq list | fzy);
+}
+
+# easy zlib decompression
+# via: Building Git
+#
+# cat .git/objects/XX/1234 | inflate
+inflate () {
+    ruby -r zlib -e "STDOUT.write Zlib::Inflate.inflate(STDIN.read)"
 }
